654cb31bcfeab35dd573ef96a8542ca5a768395a
Tiest Vilee <tiestvilee@fastmail.com.au>
2015-02-20 13:37:18 +0000

    TV: get rid of cheat text, and start actually returning stuff. put in assertion for basePath (though I'd really like to be able to specify this...)

--- test/com/utterlyswagger/petshop/TestPetShopSwagger.java
+++ test/com/utterlyswagger/petshop/TestPetShopSwagger.java
@@ -7,14 +7,17 @@ import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.AllOf.allOf;
 
 public abstract class TestPetShopSwagger {
 
     @Test
-    public void definesSwaggerVersion() throws Exception {
+    public void definesSwaggerVersionAndBasePath() throws Exception {
         assertThat(
             getSwagger(),
-            PathAssertions.stringInPath(is("2.0"), "swagger"));
+            allOf(
+                PathAssertions.stringInPath(is("2.0"), "swagger"),
+                PathAssertions.stringInPath(is("basePath"), "/v2")));
     }
 
     protected abstract Map<String, Object> getSwagger() throws Exception;
